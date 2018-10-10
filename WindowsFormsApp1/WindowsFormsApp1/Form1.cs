bcad2c4c259ef29e702f5a95817fe3c67cca66a3
t-hashimoto <t-hashimoto@chancelab.jp>
2018-10-11 08:57:45 +0900

    コードレビュー指摘もれ対応＆タブフォーカス移動順設定＆未使用クラス削除

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,12 +12,19 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        public Form1()
+
+        /// <summary>
+		/// コンストラクター
+		/// </summary>
+		public Form1()
         {
             InitializeComponent();
         }
 
-        static void Main()
+        /// <summary>
+		/// メイン処理
+		/// </summary>
+		static void Main()
         {
             Application.Run(new Form1());          
         }
@@ -31,7 +38,7 @@ namespace WindowsFormsApp1
         /// <param name="e"></param>
         private void Form1_Load(object sender, EventArgs e)
         {
-            // メインフォーム初期表示
+            // フォーム状態初期化
             Clear();
         }
 
@@ -42,7 +49,8 @@ namespace WindowsFormsApp1
         /// <param name="e"></param>
         private void clearButton_Click(object sender, EventArgs e)
         {
-            Clear();
+			// フォーム状態初期化
+			Clear();
         }
 
         /// <summary>
@@ -52,11 +60,10 @@ namespace WindowsFormsApp1
         /// <param name="e"></param>
         private void endButton_Click(object sender, EventArgs e)
         {
-            this.Close();
+			// フォームを閉じる
+			this.Close();
         }
 
-        
-        
         /// <summary>
         /// 計算ボタンクリックイベント
         /// </summary>
@@ -71,7 +78,7 @@ namespace WindowsFormsApp1
             string message = InputCheck(batsVal, hitsVal);
 
             // ⇒入力チェックの結果、エラーがあればメッセージをダイアログに出して処理終了
-            if (!(message == null))
+            if (!string.IsNullOrWhiteSpace(message))
             {
                 MessageBox.Show(message);
                 return;
@@ -86,6 +93,7 @@ namespace WindowsFormsApp1
             // 打率計算を実施       
             else
             {
+				// 打数,安打数を数値変換 ※入力値チェックが完了しているので数値変換時のエラーの心配なし
                 double batsDoubleVal = double.Parse(batsVal);
                 double hitsDoubleVal = double.Parse(hitsVal);
 
@@ -98,26 +106,40 @@ namespace WindowsFormsApp1
             }
         }
 
-        #endregion
+		/// <summary>
+		/// テキストボックスフォーカスイベント
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void TextBox_Focus(object sender, EventArgs e)
+		{
+			// イベント発生元(sender)をテキストボックスにキャスト
+			TextBox txt = (TextBox)sender;
 
-        #region privateメソッド
+			// テキストボックスを全選択状態とする
+			txt.SelectAll();
+		}
 
-        /// <summary>
-        /// クリアメソッド
-        /// </summary>
-        private void Clear()
+		#endregion
+
+		#region privateメソッド
+
+		/// <summary>
+		/// クリアメソッド
+		/// </summary>
+		private void Clear()
         {
             batsText.Text = "0";
             hitsText.Text = "0";
             averageText.Text = "";
         }
 
-        /// <summary>
-        /// 打率表示整形メソッド
-        /// </summary>
-        /// <param name="averageVal"></param>
-        /// <returns>「x 割 y 分 z 厘」に表示整形し、打率表示整形メソッドに返す</returns>
-        private string AveFormat(double averageVal)
+		/// <summary>
+		/// 打率表示整形メソッド
+		/// </summary>
+		/// <param name="averageVal">打率(小数値)</param>
+		/// <returns>打率(小数値)を「x 割 y 分 z 厘」書式で整形した文字列</returns>
+		private string AveFormat(double averageVal)
         {
             // 書式フォーマット
             string formatStr = "{0} 割 {1} 分 {2} 厘";
@@ -136,22 +158,22 @@ namespace WindowsFormsApp1
             }
 
             // 打率数値をゼロ埋めし、string型に代入
-            string aveText = string.Format("{0:f3}\r\n", ave);
+            string aveText = string.Format("{0:f3}", ave);
 
             // 打率数値をテキストにし、文字列の配置で数値を取り出す
             char ave1 = aveText[2];
             char ave2 = aveText[3];
             char ave3 = aveText[4];
-            //「x 割 y 分 z 厘」に表示整形し、打率表示整形メソッドに返す
-            return string.Format(formatStr, ave1, ave2, ave3);
+			//「x 割 y 分 z 厘」に表示整形し、打率表示整形メソッドの戻り値として返す
+			return string.Format(formatStr, ave1, ave2, ave3);
         }
 
         /// <summary>
         /// 打率計算メソッド
         /// </summary>
-        /// <param name="batNum"></param>
-        /// <param name="hitNum"></param>
-        /// <returns>打率を小数点第四位で四捨五入し、打率計算メソッドの戻り値として返す</returns>
+        /// <param name="batNum">打数</param>
+        /// <param name="hitNum">安打数</param>
+        /// <returns>打率(小数点第四位で四捨五入)</returns>
         private double CalcAverage(double batNum, double hitNum)
         {
             // 打率を計算
@@ -201,8 +223,8 @@ namespace WindowsFormsApp1
             //「打数」の入力内容が正の整数以外の場合
             // ※文字、小数点付きの数値をエラーにする
             // メッセージ：「打数、安打数は正の整数で入力してください」をダイアログに表示して処理終了
-            int number1 = 0;
-            bool canConvert = int.TryParse(batsVal, out number1);
+            int batsNumber = 0;
+            bool canConvert = int.TryParse(batsVal, out batsNumber);
             if (!canConvert)
             {
                 return "打数、安打数は正の整数で入力してください";
@@ -211,9 +233,9 @@ namespace WindowsFormsApp1
             // 「安打数」の入力内容が正の整数以外の場合
             // ※文字、小数点付きの数値をエラーにする
             // メッセージ：「打数、安打数は正の整数で入力してください」をダイアログに表示して処理終了
-            int number2 = 0;
+            int hitsNumber = 0;
 
-            canConvert = int.TryParse(hitsVal, out number2);
+            canConvert = int.TryParse(hitsVal, out hitsNumber);
             if (!canConvert)
             {
                 return "打数、安打数は正の整数で入力してください";
@@ -222,10 +244,8 @@ namespace WindowsFormsApp1
 
             // ※マイナス数値をエラーにする
             // メッセージ：「打数、安打数は正の整数で入力してください」をダイアログに表示して処理終了
-            double batNum = double.Parse(batsVal);
-            double hitNum = double.Parse(hitsVal);
 
-            if (batNum < 0 || hitNum < 0)
+            if (batsNumber < 0 || hitsNumber < 0)
             {
                 return "打数、安打数は正の整数で入力してください";
 
@@ -236,17 +256,16 @@ namespace WindowsFormsApp1
             //「打数」＜「安打数」の場合
             // メッセージ：「安打数は打数以下の値を入力してください」
 
-            if (batNum < hitNum)
+            if (batsNumber < hitsNumber)
             {
                 return "安打数は打数以下の値を入力してください";
             }
 
             // ここまで来たのでエラーなし＝空白を返却
-            return null;
+            return "";
         }
 
         #endregion
-
 
     }
 }
